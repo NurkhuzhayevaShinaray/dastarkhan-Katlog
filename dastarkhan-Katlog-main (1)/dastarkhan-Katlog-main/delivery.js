9fced01713c4cedae834e93aeb9eb36bec7eb0e3
Твоё имя <  >
2025-10-20 17:26:16 +0600

    starting

---
+++ dastarkhan-Katlog-main (1)/dastarkhan-Katlog-main/delivery.js
@@ -0,0 +1,55 @@
+document.getElementById("deliveryForm").addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  const name = document.getElementById("name").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+  const address = document.getElementById("address").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message");
+
+  let errors = [];
+
+  if (name === "") errors.push("Введите имя");
+  if (address === "") errors.push("Введите адрес");
+  if (phone === "" || !/^\+?\d{10,13}$/.test(phone)) errors.push("Введите корректный телефон");
+  if (email === "" || !email.includes("@")) errors.push("Введите корректный email");
+
+  if (errors.length > 0) {
+    message.style.color = "red";
+    message.textContent = errors.join(", ");
+  } else {
+    message.style.color = "green";
+    message.textContent = "Спасибо! Ваша заявка отправлена.";
+    document.getElementById("deliveryForm").reset();
+  }
+});
+
+
+
+
+const sumInput = document.getElementById("sum");
+const costDisplay = document.getElementById("cost");
+
+const deliveryOptions = [
+  { min: 0, max: 5000, cost: 1000 },
+  { min: 5000, max: 10000, cost: 500 },
+  { min: 10000, max: Infinity, cost: 0 }
+];
+
+sumInput.addEventListener("input", () => {
+  const sum = Number(sumInput.value);
+  let deliveryCost = 0;
+
+  for (let option of deliveryOptions) {
+    if (sum >= option.min && sum < option.max) {
+      deliveryCost = option.cost;
+      break;
+    }
+  }
+
+  costDisplay.textContent =
+    deliveryCost === 0
+      ? "фБесплатная доставка"
+      : `Стоимость доставки: ${deliveryCost} ₸`;
+});
+
---
+++ dastarkhan-Katlog-main (1)/dastarkhan-Katlog-main/reviews.js
@@ -0,0 +1,40 @@
+document.getElementById("reviewForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = this.querySelector('input').value.trim();
+  const rating = this.querySelector('#rating').value;
+  const text = this.querySelector('textarea').value.trim();
+
+  if (!name || !text) return alert("Заполните все поля!");
+
+  const card = document.createElement("div");
+  card.className = "review-card";
+  card.innerHTML = `
+    <h3>${name}</h3>
+    <p class="stars">${rating}</p>
+    <p>${text}</p>
+  `;
+
+ 
+  const list = document.getElementById("reviewsslist");
+  list.appendChild(card);
+
+
+  this.reset();
+
+ 
+  card.style.opacity = "0";
+  card.style.transform = "scale(0.9)";
+  setTimeout(() => {
+    card.style.transition = "all 0.4s ease";
+    card.style.opacity = "1";
+    card.style.transform = "scale(1)";
+  }, 50);
+
+});
+
+
+
+
+
+
